642a4b7a14b99aa2f2eb75b49ee91f6f3b3f03cf
Rohitpipare06 <100850761+Rohitpipare06@users.noreply.github.com>
2022-03-18 19:28:58 +0530

    Add files via upload

---
+++ whatisoutputque9.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#define NULL0
+int main()
+{
+	while(NULL==0)
+	{
+		printf("While loop");
+		break;
+	}
+	return 0;
+}
+
+
+//Explnation:- The conditon is true so it returns 1 and execute the block only once
